9edd1a781715a7d4e8e6817c1ac69fa37e6edf8a
prashant <prashantgtm@hotmail.com>
2016-01-27 19:25:20 +0545

    added details for adp,pepsico,stryker,cc

--- sortfiletype.py
+++ sortfiletype.py
@@ -5,7 +5,7 @@ class SortFileType:
 
 	def getFileType(self,clientName,fileName):
 		
-		if clientName == "Correctcare":
+		if clientName == 'Correctcare':
 			if re.search('^jobs\.[0-9.]+\.csv',fileName):
 				fileType = 'req'
 				rawTableName = 'zzz_cc_req_raw'
@@ -17,7 +17,7 @@ class SortFileType:
 			else:
 				sys.exit("Unknown file")
 				
-		elif clientName == "Pepsico":
+		elif clientName == 'Pepsico':
 			if re.search('^NEW\sBB\sCandidate\s',fileName):
 				fileType = 'candidate'
 				rawTableName = 'zzz_pepsico_candidate_raw'
@@ -29,5 +29,29 @@ class SortFileType:
 			else:
 				sys.exit("Unknown file")
 			
+		elif clientName == 'ADP':
+			if re.search('^ADP~adpbroadbean_RM_Can_[0-9_]+\.csv',fileName):
+				fileType = 'candidate'
+				rawTableName = 'zzz_adp_candidate_raw'
+				return fileType,rawTableName
+			elif (re.search('^ADP~adpbroadbean_RM_Req_[0-9_]+\.csv',fileName)):
+				fileType = 'req'
+				rawTableName = 'zzz_adp_req_raw'
+				return fileType,rawTableName
+			else:
+				sys.exit("Unknown file")
+		
+		elif clientName == 'Stryker':
+			if re.search('^BDAS\sCandidate\s[0-9_A-Za-z]',fileName):
+				fileType = 'candidate'
+				rawTableName = 'zzz_adp_candidate_raw'
+				return fileType,rawTableName
+			elif (re.search('^BDAS\sReqToDate\s[0-9_A-Za-z]',fileName)):
+				fileType = 'req'
+				rawTableName = 'zzz_adp_req_raw'
+				return fileType,rawTableName
+			else:
+				sys.exit("Unknown file")
+
 		else:
 			sys.exit("Unknown client")
